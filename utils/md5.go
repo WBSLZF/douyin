da046215cbed9ddde6118614073dd76284de8ce5
WBSLZF <1439163529@qq.com>
2023-08-14 14:33:03 +0800

    Feature:Add md5 util

---
+++ utils/md5.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+// 输入数据并返回MD5哈希值的16进制字符串表示作为函数返回结果
+func Md5Encode(data string) string {
+	h := md5.New()                     //创建一个MD5哈希对象
+	h.Write([]byte(data))              //将字节数组写入MD5哈希对象
+	tempStr := h.Sum(nil)              //计算并返回MD5哈希对象的哈希值，参数nil表示不添加任何额外的数据
+	return hex.EncodeToString(tempStr) //将MD5的哈希值的16进制字符串表示函数返回的结果
+}
+
+// 加密
+func MakePassWord(plainwd string) string {
+	return Md5Encode(plainwd)
+}
+
+// 解密
+func ValidPassWord(plainwd, password string) bool {
+	encode_password := Md5Encode(plainwd)
+	return encode_password == password
+}
