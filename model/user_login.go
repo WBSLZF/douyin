f389b9c22ab1b2a578144bd34531fef44df204b9
Robot-2020 <1055570860@qq.com>
2023-08-14 16:16:19 +0800

    database init change by wza

--- model/user_login.go
+++ model/user_login.go
@@ -1,8 +1,8 @@
 package model
 
 type UserLogin struct {
-	Id         int
+	Id         int64
 	UserCount  string `gorm:"notnull"`
 	PassWord   string `gorm:"notnull"`
-	UserInfoId int    //账户密码的一对一关系
+	UserInfoId int64  //账户密码的一对一关系
 }
