0067eaab764ff31a38a4b9c61f2a438cf7fe322d
WBSLZF <1439163529@qq.com>
2023-08-16 15:40:17 +0800

    Bug:用户组测以及登录失败状态码修改

--- controller/user.go
+++ controller/user.go
@@ -43,7 +43,7 @@ func Register(c *gin.Context) {
 	userLoginData, err := service.UserLogin{}.Register(username, password)
 
 	if err != nil {
-		c.JSON(http.StatusOK, UserLoginResponse{
+		c.JSON(http.StatusConflict, UserLoginResponse{
 			Response: model.Response{StatusCode: 1, StatusMsg: err.Error()},
 		})
 		return
@@ -70,7 +70,7 @@ func Login(c *gin.Context) {
 	password := c.Query("password")
 	userLoginData, err := service.UserLogin{}.Login(username, password)
 	if err != nil {
-		c.JSON(http.StatusOK, UserLoginResponse{
+		c.JSON(http.StatusUnauthorized, UserLoginResponse{
 			Response: model.Response{StatusCode: 1, StatusMsg: err.Error()},
 		})
 		return
